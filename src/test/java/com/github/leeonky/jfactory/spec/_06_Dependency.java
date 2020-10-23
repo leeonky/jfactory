0450b271cc505feb2f89de0d8f2396b09524ca70
Liangchen <leeonky@gmail.com>
2020-10-23 21:24:51 +0800

    Fix mutation test

--- src/test/java/com/github/leeonky/jfactory/spec/_06_Dependency.java
+++ src/test/java/com/github/leeonky/jfactory/spec/_06_Dependency.java
@@ -13,6 +13,29 @@ import static org.assertj.core.api.Assertions.fail;
 
 public class _06_Dependency {
     private FactorySet factorySet = new FactorySet();
+    private int intValue = 1;
+
+    @Test
+    void should_cache_produce_value() {
+        factorySet.factory(Bean.class).spec(instance -> instance.spec()
+                .property("stringValue").value(() -> String.valueOf(intValue++))
+        );
+        factorySet.factory(Beans.class)
+                .spec(instance -> instance.spec()
+                        .property("bean1").asDefault()
+                        .property("bean2").asDefault()
+                        .property("bean2.stringValue").dependsOn("bean1.stringValue", v -> v)
+                        .property("bean3").asDefault()
+                        .property("bean3.stringValue").dependsOn("bean1.stringValue", v -> v)
+                );
+
+        Beans beans = factorySet.create(Beans.class);
+
+        assertThat(beans.bean2.stringValue)
+                .isEqualTo(beans.bean3.stringValue)
+                .isEqualTo(beans.bean1.stringValue)
+                .isEqualTo("1");
+    }
 
     @Getter
     @Setter
