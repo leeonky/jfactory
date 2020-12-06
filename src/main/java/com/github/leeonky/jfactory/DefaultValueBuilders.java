d6fd0d0409fb23b8ec145ef28e901b9eed250537
Liangchen <leeonky@gmail.com>
2020-12-06 15:42:50 +0800

    Support more default value types

--- src/main/java/com/github/leeonky/jfactory/DefaultValueBuilders.java
+++ src/main/java/com/github/leeonky/jfactory/DefaultValueBuilders.java
@@ -2,19 +2,47 @@ package com.github.leeonky.jfactory;
 
 import com.github.leeonky.util.BeanClass;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.*;
+import java.time.temporal.ChronoUnit;
+import java.util.*;
 
 import static java.util.Optional.ofNullable;
 
 class DefaultValueBuilders {
+    private static final LocalDate LOCAL_DATE_START = LocalDate.parse("1996-01-23");
+    private static final LocalDateTime LOCAL_DATE_TIME_START = LocalDateTime.parse("1996-01-23T00:00:00");
+    private static final LocalTime LOCAL_TIME_START = LocalTime.parse("00:00:00");
+    private static final Instant INSTANT_START = Instant.parse("1996-01-23T00:00:00Z");
     private final Map<Class<?>, DefaultValueBuilder<?>> defaultValueBuilders = new HashMap<>();
 
     public DefaultValueBuilders() {
         defaultValueBuilders.put(String.class, new DefaultStringBuilder());
         defaultValueBuilders.put(Integer.class, new DefaultIntegerBuilder());
         defaultValueBuilders.put(int.class, defaultValueBuilders.get(Integer.class));
+        defaultValueBuilders.put(Short.class, new DefaultShortBuilder());
+        defaultValueBuilders.put(short.class, defaultValueBuilders.get(Short.class));
+        defaultValueBuilders.put(Byte.class, new DefaultByteBuilder());
+        defaultValueBuilders.put(byte.class, defaultValueBuilders.get(Byte.class));
+        defaultValueBuilders.put(Long.class, new DefaultLongBuilder());
+        defaultValueBuilders.put(long.class, defaultValueBuilders.get(Long.class));
+        defaultValueBuilders.put(Float.class, new DefaultFloatBuilder());
+        defaultValueBuilders.put(float.class, defaultValueBuilders.get(Float.class));
+        defaultValueBuilders.put(Double.class, new DefaultDoubleBuilder());
+        defaultValueBuilders.put(double.class, defaultValueBuilders.get(Double.class));
+        defaultValueBuilders.put(Boolean.class, new DefaultBooleanBuilder());
+        defaultValueBuilders.put(boolean.class, defaultValueBuilders.get(Boolean.class));
+        defaultValueBuilders.put(BigInteger.class, new DefaultBigIntegerBuilder());
+        defaultValueBuilders.put(BigDecimal.class, new DefaultBigDecimalBuilder());
+        defaultValueBuilders.put(UUID.class, new DefaultUUIDBuilder());
+        defaultValueBuilders.put(Date.class, new DefaultDateBuilder());
+        defaultValueBuilders.put(Instant.class, new DefaultInstantBuilder());
+        defaultValueBuilders.put(LocalDate.class, new DefaultLocalDateBuilder());
+        defaultValueBuilders.put(LocalTime.class, new DefaultLocalTimeBuilder());
+        defaultValueBuilders.put(LocalDateTime.class, new DefaultLocalDateTimeBuilder());
+        defaultValueBuilders.put(OffsetDateTime.class, new DefaultOffsetDateTimeBuilder());
+        defaultValueBuilders.put(ZonedDateTime.class, new DefaultZoneDateTimeBuilder());
     }
 
     @SuppressWarnings("unchecked")
@@ -30,11 +58,139 @@ class DefaultValueBuilders {
         }
     }
 
+    public static class DefaultLongBuilder implements DefaultValueBuilder<Long> {
+
+        @Override
+        public <T> Long create(BeanClass<T> beanType, SubInstance instance) {
+            return (long) instance.getSequence();
+        }
+    }
+
     public static class DefaultIntegerBuilder implements DefaultValueBuilder<Integer> {
 
         @Override
         public <T> Integer create(BeanClass<T> beanType, SubInstance instance) {
             return instance.getSequence();
+        }
+    }
+
+    public static class DefaultShortBuilder implements DefaultValueBuilder<Short> {
+
+        @Override
+        public <T> Short create(BeanClass<T> beanType, SubInstance instance) {
+            return (short) instance.getSequence();
+        }
+    }
+
+    public static class DefaultByteBuilder implements DefaultValueBuilder<Byte> {
+
+        @Override
+        public <T> Byte create(BeanClass<T> beanType, SubInstance instance) {
+            return (byte) instance.getSequence();
+        }
+    }
+
+    public static class DefaultDoubleBuilder implements DefaultValueBuilder<Double> {
+
+        @Override
+        public <T> Double create(BeanClass<T> beanType, SubInstance instance) {
+            return (double) instance.getSequence();
+        }
+    }
+
+    public static class DefaultFloatBuilder implements DefaultValueBuilder<Float> {
+
+        @Override
+        public <T> Float create(BeanClass<T> beanType, SubInstance instance) {
+            return (float) instance.getSequence();
+        }
+    }
+
+    public static class DefaultBooleanBuilder implements DefaultValueBuilder<Boolean> {
+
+        @Override
+        public <T> Boolean create(BeanClass<T> beanType, SubInstance instance) {
+            return (instance.getSequence() % 2) == 1;
+        }
+    }
+
+    public static class DefaultBigIntegerBuilder implements DefaultValueBuilder<BigInteger> {
+
+        @Override
+        public <T> BigInteger create(BeanClass<T> beanType, SubInstance instance) {
+            return BigInteger.valueOf(instance.getSequence());
+        }
+    }
+
+    public static class DefaultBigDecimalBuilder implements DefaultValueBuilder<BigDecimal> {
+
+        @Override
+        public <T> BigDecimal create(BeanClass<T> beanType, SubInstance instance) {
+            return BigDecimal.valueOf(instance.getSequence());
+        }
+    }
+
+    public static class DefaultUUIDBuilder implements DefaultValueBuilder<UUID> {
+
+        @Override
+        public <T> UUID create(BeanClass<T> beanType, SubInstance instance) {
+            return UUID.fromString(String.format("00000000-0000-0000-0000-%012d", instance.getSequence()));
+        }
+    }
+
+    public static class DefaultDateBuilder implements DefaultValueBuilder<Date> {
+
+        @Override
+        public <T> Date create(BeanClass<T> beanType, SubInstance instance) {
+            return Date.from(INSTANT_START.plus(instance.getSequence(), ChronoUnit.DAYS));
+        }
+    }
+
+    public static class DefaultInstantBuilder implements DefaultValueBuilder<Instant> {
+
+        @Override
+        public <T> Instant create(BeanClass<T> beanType, SubInstance instance) {
+            return INSTANT_START.plusSeconds(instance.getSequence());
+        }
+    }
+
+    public static class DefaultLocalTimeBuilder implements DefaultValueBuilder<LocalTime> {
+
+        @Override
+        public <T> LocalTime create(BeanClass<T> beanType, SubInstance instance) {
+            return LOCAL_TIME_START.plusSeconds(instance.getSequence());
+        }
+    }
+
+    public static class DefaultLocalDateBuilder implements DefaultValueBuilder<LocalDate> {
+
+        @Override
+        public <T> LocalDate create(BeanClass<T> beanType, SubInstance instance) {
+            return LOCAL_DATE_START.plusDays(instance.getSequence());
+        }
+    }
+
+    public static class DefaultLocalDateTimeBuilder implements DefaultValueBuilder<LocalDateTime> {
+
+        @Override
+        public <T> LocalDateTime create(BeanClass<T> beanType, SubInstance instance) {
+            return LOCAL_DATE_TIME_START.plusSeconds(instance.getSequence());
+        }
+    }
+
+    public static class DefaultOffsetDateTimeBuilder implements DefaultValueBuilder<OffsetDateTime> {
+
+        @Override
+        public <T> OffsetDateTime create(BeanClass<T> beanType, SubInstance instance) {
+            return INSTANT_START.plusSeconds(instance.getSequence()).atZone(ZoneId.systemDefault()).toOffsetDateTime();
+        }
+    }
+
+    public static class DefaultZoneDateTimeBuilder implements DefaultValueBuilder<ZonedDateTime> {
+
+        @Override
+        public <T> ZonedDateTime create(BeanClass<T> beanType, SubInstance instance) {
+            return INSTANT_START.plusSeconds(instance.getSequence()).atZone(ZoneId.systemDefault());
         }
     }
 
--- src/test/java/com/github/leeonky/jfactory/DefaultValueBuildersTest.java
+++ src/test/java/com/github/leeonky/jfactory/DefaultValueBuildersTest.java
@@ -5,6 +5,11 @@ import com.github.leeonky.util.PropertyWriter;
 import org.junit.jupiter.api.Test;
 
 import java.lang.annotation.Annotation;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.*;
+import java.util.Date;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
@@ -12,20 +17,117 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 class DefaultValueBuildersTest {
 
     @Test
-    void create_string() {
+    void default_string() {
         assertValue(String.class, 1, "str", "str#1");
         assertValue(String.class, 3, "name", "name#3");
     }
 
     @Test
-    void create_int() {
+    void default_int() {
         assertValue(int.class, 1, 1);
         assertValue(Integer.class, 3, 3);
     }
 
     @Test
+    void default_short() {
+        assertValue(short.class, 1, (short) 1);
+        assertValue(Short.class, 2, (short) 2);
+    }
+
+    @Test
+    void default_byte() {
+        assertValue(byte.class, 1, (byte) 1);
+        assertValue(Byte.class, 2, (byte) 2);
+    }
+
+    @Test
+    void default_long() {
+        assertValue(long.class, 1, 1L);
+        assertValue(Long.class, 2, 2L);
+    }
+
+    @Test
+    void default_float() {
+        assertValue(float.class, 1, 1.0f);
+        assertValue(Float.class, 2, 2.0f);
+    }
+
+    @Test
+    void default_double() {
+        assertValue(double.class, 1, 1.0);
+        assertValue(Double.class, 2, 2.0);
+    }
+
+    @Test
     void should_raise_error_when_invalid_generic_args() {
         assertThrows(IllegalStateException.class, () -> new InvalidGenericArgDefaultValueBuilder<>().getType());
+    }
+
+    @Test
+    void default_boolean() {
+        assertValue(boolean.class, 1, true);
+        assertValue(Boolean.class, 2, false);
+        assertValue(boolean.class, 3, true);
+    }
+
+    @Test
+    void default_big_int() {
+        assertValue(BigInteger.class, 1, BigInteger.valueOf(1));
+        assertValue(BigInteger.class, 2, BigInteger.valueOf(2));
+    }
+
+    @Test
+    void default_big_decimal() {
+        assertValue(BigDecimal.class, 1, BigDecimal.valueOf(1));
+        assertValue(BigDecimal.class, 2, BigDecimal.valueOf(2));
+    }
+
+    @Test
+    void default_uuid() {
+        assertValue(UUID.class, 1, UUID.fromString("00000000-0000-0000-0000-000000000001"));
+        assertValue(UUID.class, 2, UUID.fromString("00000000-0000-0000-0000-000000000002"));
+    }
+
+    @Test
+    void default_instant() {
+        assertValue(Instant.class, 1, Instant.parse("1996-01-23T00:00:01Z"));
+        assertValue(Instant.class, 2, Instant.parse("1996-01-23T00:00:02Z"));
+    }
+
+    @Test
+    void default_date() {
+        assertValue(Date.class, 1, Date.from(Instant.parse("1996-01-24T00:00:00Z")));
+        assertValue(Date.class, 2, Date.from(Instant.parse("1996-01-25T00:00:00Z")));
+    }
+
+    @Test
+    void default_local_time() {
+        assertValue(LocalTime.class, 1, LocalTime.parse("00:00:01"));
+        assertValue(LocalTime.class, 2, LocalTime.parse("00:00:02"));
+    }
+
+    @Test
+    void default_local_date() {
+        assertValue(LocalDate.class, 1, LocalDate.parse("1996-01-24"));
+        assertValue(LocalDate.class, 2, LocalDate.parse("1996-01-25"));
+    }
+
+    @Test
+    void default_local_date_time() {
+        assertValue(LocalDateTime.class, 1, LocalDateTime.parse("1996-01-23T00:00:01"));
+        assertValue(LocalDateTime.class, 2, LocalDateTime.parse("1996-01-23T00:00:02"));
+    }
+
+    @Test
+    void default_offset_date_time() {
+        assertValue(OffsetDateTime.class, 1, Instant.parse("1996-01-23T00:00:01Z").atZone(ZoneId.systemDefault()).toOffsetDateTime());
+        assertValue(OffsetDateTime.class, 2, Instant.parse("1996-01-23T00:00:02Z").atZone(ZoneId.systemDefault()).toOffsetDateTime());
+    }
+
+    @Test
+    void default_zoned_date_time() {
+        assertValue(ZonedDateTime.class, 1, Instant.parse("1996-01-23T00:00:01Z").atZone(ZoneId.systemDefault()));
+        assertValue(ZonedDateTime.class, 2, Instant.parse("1996-01-23T00:00:02Z").atZone(ZoneId.systemDefault()));
     }
 
     private void assertValue(Class<?> type, int sequence, String property, Object expected) {
