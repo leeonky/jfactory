f29b8854061980c6ea1c21e6f7d05f89c1bcc6dc
Liangchen <leeonky@gmail.com>
2021-11-28 12:00:43 +0800

    Add LICENSE

--- src/main/java/com/github/leeonky/jfactory/PropertyValue.java
+++ src/main/java/com/github/leeonky/jfactory/PropertyValue.java
@@ -1,9 +1,6 @@
 package com.github.leeonky.jfactory;
 
 public interface PropertyValue {
-    static PropertyValue table(String table) {
-        return new TablePropertyValue(table);
-    }
 
     <T> Builder<T> setToBuilder(String property, Builder<T> builder);
 }
--- src/test/java/com/github/leeonky/jfactory/DefaultBuilderTest.java
+++ src/test/java/com/github/leeonky/jfactory/DefaultBuilderTest.java
@@ -1,19 +1,12 @@
 package com.github.leeonky.jfactory;
 
-import com.github.leeonky.dal.extension.assertj.DALAssert;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.experimental.Accessors;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
-
-import static com.github.leeonky.dal.extension.assertj.DALAssert.expect;
-import static com.github.leeonky.jfactory.PropertyValue.table;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DefaultBuilderTest {
     private JFactory jFactory = new JFactory();
@@ -34,16 +27,8 @@ class DefaultBuilderTest {
 
     @Getter
     @Setter
-    @Accessors(chain = true)
-    public static class Item {
-        private String value, value2;
-    }
-
-    @Getter
-    @Setter
     public static class Bean {
         private String stringValue;
-        private List<Item> list;
     }
 
     @Getter
@@ -105,51 +90,6 @@ class DefaultBuilderTest {
         void should_not_equal_for_different_trait() {
             assertThat(builder(Bean.class, "string", "trait1"))
                     .isNotEqualTo(builder(Bean.class, "string", "trait2"));
-        }
-    }
-
-    @Nested
-    class Table {
-        private final Builder<Bean> builder = jFactory.type(Bean.class);
-
-        @Test
-        void table_with_only_header_equals_empty_property() {
-            expectTable("| value |").match("[]");
-        }
-
-        @Test
-        void table_1_x_1() {
-            expectTable("| value |\n" +
-                    "| hello |")
-                    .should("value: ['hello']");
-        }
-
-        @Test
-        void table_2_x_1() {
-            expectTable("| value |\n" +
-                    "| hello |\n" +
-                    "| world |")
-                    .should("value: ['hello' 'world']");
-        }
-
-        @Test
-        void table_2_x_2() {
-            expectTable("| value | value2 |\n" +
-                    "| hello | Tom |\n" +
-                    "| world | Jerry |")
-                    .should("value: ['hello' 'world']")
-                    .should("value2: ['Tom' 'Jerry']");
-        }
-
-        @Test
-        void invalid_table_too_many_cells() {
-            assertThat(assertThrows(IllegalArgumentException.class, () ->
-                    builder.propertyValue("list", table("| value |\n" +
-                            "| hello | world |")))).hasMessage("Invalid table at row: 0, different size of cells and headers.");
-        }
-
-        private DALAssert expectTable(String table) {
-            return expect(builder.propertyValue("list", table(table)).create().getList());
         }
     }
 }
